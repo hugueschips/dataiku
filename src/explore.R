bb981ed7590afb6f5256a44322e6b9c3af5f5c23
Daniel Durrenberger <dd.durrenberger@gmail.com>
2020-03-10 15:24:48 +0100

    more comments

--- src/explore.R
+++ src/explore.R
@@ -13,28 +13,27 @@ library(xgboost)
 library(tibble)
 library(ggplot2)
 library(pROC)
-library(earth)
 library(nnet)
-library(rocc)
 library(caret)
 
-# Project metadata
-method = 'xgbTree'
-# method = 'mlpKerasDecayCost'
-# method = 'nnet'
-# method = 'rocc'
+#################### Wether or not you want to train again or load results
+trainAgain <- FALSE
 
-# Files
+#################### Choice of learning algorith
+######################## 2 options are provided : xgboost or neural network
+method = 'xgbTree'
+# method = 'nnet'
+
+####################  Load Files
 trainFile <- "./data/census_income_learn.csv"
 testFile <- './data/census_income_test.csv'
-
-# Load Files
 trainDataRaw <- read.csv(file = trainFile, header = FALSE) %>% as.tibble
 testDataRaw <- read.csv(file = testFile, header = FALSE) %>% as.tibble
 trainDataRaw %>% head #See what it looks like
 
-# Clean data
-## Convert all non numeric into dummy variables and keep numerics unchanged
+####################  Clean data
+######################## Convert all non numeric into dummy variables 
+######################## and keep numerics unchanged
 customPreprocess2 <- function(trainDataRaw) {
   trainDataClean <- trainDataRaw %>%
     mutate(class = if_else(as.numeric(V42)==2, 'over50', 'below50') %>% factor) %>%
@@ -52,38 +51,16 @@ allData <- bind_rows(
   testDataRaw %>% mutate(train = 0)) %>%
   customPreprocess2
 
-# Remove columns with near zero variance.
+######################## Remove columns with near zero variance
+######################## it is not needed by algorithms like xgboost
+######################## but it speeds up the training
 nzv <- nearZeroVar(allData)
 allData <- allData[,-nzv]
 
 allData %>% head #See result
 
-# trainDataClean <- trainDataRaw %>% customPreprocess2
-# testDataClean <- testDataRaw %>% customPreprocess2
-
-
-
-# Explore data
-# summary(trainDataClean$X73)
-# snippet <- trainDataClean %>% 
-#                         sample_n(100) %>%
-#                         select_if(is.numeric)
-# graph1 <- ggplot(trainDataClean, aes(x=X73, y=over50k)) + geom_bar()
-# graph1
-# 
-# 
-# colNames <- colnames(trainDataClean)
-# ggplot(trainDataClean %>% sample_n(1000), aes(x=X0.6, y=as.numeric(class)))+
-#   # geom_point(size=2, alpha=0.4)+
-#   stat_smooth(method="loess", colour="blue", size=1.5)+
-#   # xlab("Age")+
-#   ylab("Probability of earning over 50K")+
-#   theme_bw()
-
-
-
-# Split data
-## One half of training set is used to train
+##################### Split data
+######################## One half of training set is used to train
 xytrain <- allData %>%
   filter(train == 1) %>%
   select(-train) %>%
@@ -91,27 +68,27 @@ xytrain <- allData %>%
   sample_frac(.5) %>%
   ungroup
 
-## The other half for calibration
+######################## The other half for calibration
 xycal <- allData %>%
   filter(train == 1) %>%
   select(-train) %>%
   anti_join(xytrain) %>%
   ungroup
 
-## Test set is left untouched
+######################## Test set is left untouched
 xytest <- allData %>%
   filter(train == 0) %>%
   select(-train) %>%
   ungroup
 
-## Check class balance
+######################## Check class balance
 xytrain$class %>% summary
 xycal$class %>% summary
 xytest$class %>% summary
 xytrain %>% dim
 
-# Train Model
-## Settings
+#################### Train Model
+######################## Settings
 fitControl <- trainControl(
   method = "repeatedcv",
   number = 5,
@@ -120,7 +97,8 @@ fitControl <- trainControl(
   classProbs = TRUE, 
   verboseIter = TRUE)
 
-## training
+######################## Training
+if (trainAgain) {
 fit <- train(
   x = xytrain %>% select(-class) %>% as.matrix,
   y = xytrain$class,
@@ -130,32 +108,45 @@ fit <- train(
   metric = 'ROC',
   verbose = TRUE
 )
-
-# Results
-## Training performance
 fit$results %>% arrange(-ROC) %>% head
+saveRDS(fit, file = 'model_new.RData')
+}
+##################### Results
+######################## Save and load training
+if (!(trainAgain)) {fit <- readRDS('model_xgboost.RData')}
 
-## Importance of variables
+######################## Importance of variables
+############################ Here feature names are not helping, 
+############################ but usually, it is a very safe way to evaluate the model
+############################ as the most important vars should be known and studied
 varImp(fit, scale = TRUE) 
 
-# Calibration
+######################## Calibration
+############################# The aim in this section is to set the threshold 
+############################# between both of the classes. The defaut 0.5 probability
+############################# may not be the best choice. The decision should be taken
+############################# considering kappa, sensitivity, specificity, ROC curve
+############################# Accuracy should not be considered as the dataset is imbalanced
 calPredict <- predict(fit, 
                             newdata = xycal %>% select(-class) %>% as.matrix, 
                             type = 'prob') %>%
   mutate(pred = if_else(below50 > .5, 'below50', 'over50') %>% factor) %>%
   mutate(obs = xycal$class %>% factor)
 
-threshold <- 0.6
+pROC_obj_cal <- roc(calPredict$obs, calPredict$below50, ci = TRUE)
+pROC_obj_cal
+plot(pROC_obj_cal)
+
+threshold <- 0.8
 
 calPredict <- calPredict %>%
   mutate(pred = if_else(below50 > threshold, 'below50', 'over50') %>% factor)
 confusionMatrix(calPredict$obs, calPredict$pred)
 
-pROC_obj_cal <- roc(calPredict$obs, calPredict$below50, ci = TRUE)
-pROC_obj_cal
-plot(pROC_obj_cal)
-
-# Test
+######################## Test
+############################# Test set consist of untouched data, independant to training
+############################# or calibration to assess the behavior the model with 
+############################# new unknown data
 testPredict <- predict(fit, 
                       newdata = xytest %>% select(-class) %>% as.matrix, 
                       type = 'prob') %>%
@@ -171,6 +162,6 @@ pROC_obj_cal
 plot(pROC_obj_cal)
 
 
-saveRDS(fit, file = 'model_nn.RData')
+
 
 
