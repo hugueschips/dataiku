17a05cd7f07617e1e0ef831bd7f91f3f6fcbc95c
Daniel Durrenberger <dd.durrenberger@gmail.com>
2020-03-10 16:34:54 +0100

    first try for final version

--- src/explore.R
+++ src/explore.R
@@ -18,7 +18,7 @@ library(caret)
 library(corrplot)
 
 #################### Wether or not you want to train again or load results
-trainAgain <- FALSE
+trainAgain <- TRUE
 
 #################### Choice of learning algorith
 ######################## 2 options are provided : xgboost or neural network
@@ -90,6 +90,7 @@ xytest$class %>% summary
 xytrain %>% dim
 
 ######################## Produce correlation matrix of train set
+############################## (simplify names to plot something nice)
 xytrain2 <- xytrain %>% select_if(is.numeric)
 colnames(xytrain2) <- paste0('V', 1:ncol(xytrain2))
 correlationMatrix <- cor(xytrain2)
@@ -98,13 +99,19 @@ print(correlationMatrix)
 corrplot(correlationMatrix)
 ################################## find attributes that are highly
 ################################## corrected (ideally >0.75)
-highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.8)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=.80)
 ################################## print indexes of highly correlated attributes
 print(highlyCorrelated)
 ################################## print names of highly correlated attributes
 featureNames <- colnames(xytrain %>% select_if(is.numeric))
-print(featureNames[highlyCorrelated])
+highlyCorrelatedNames <- featureNames[highlyCorrelated]
+print(highlyCorrelatedNames)
 rm(xytrain2)
+
+#################### Remove higly correlated features
+xytrain <- xytrain %>% select(-highlyCorrelatedNames)
+xycal <- xycal %>% select(-highlyCorrelatedNames)
+xytest <- xytest %>% select(-highlyCorrelatedNames)
 
 #################### Train Model
 ######################## Settings
@@ -146,36 +153,37 @@ varImp(fit, scale = TRUE)
 ############################# may not be the best choice. The decision should be taken
 ############################# considering kappa, sensitivity, specificity, ROC curve
 ############################# Accuracy should not be considered as the dataset is imbalanced
+######## Predict calibration set
 calPredict <- predict(fit, 
                             newdata = xycal %>% select(-class) %>% as.matrix, 
                             type = 'prob') %>%
   mutate(pred = if_else(below50 > .5, 'below50', 'over50') %>% factor) %>%
   mutate(obs = xycal$class %>% factor)
 
+############ ROC curve for calibration set
 pROC_obj_cal <- roc(calPredict$obs, calPredict$below50, ci = TRUE)
 pROC_obj_cal
 plot(pROC_obj_cal)
-
+############# Set threshold using ROC curve
 threshold <- 0.8
-
 calPredict <- calPredict %>%
   mutate(pred = if_else(below50 > threshold, 'below50', 'over50') %>% factor)
+############# See confusion matrix with set threshold
 confusionMatrix(calPredict$obs, calPredict$pred)
 
 ######################## Test
 ############################# Test set consist of untouched data, independant to training
 ############################# or calibration to assess the behavior the model with 
 ############################# new unknown data
+########### Predict test set with chosen threshold
 testPredict <- predict(fit, 
                       newdata = xytest %>% select(-class) %>% as.matrix, 
                       type = 'prob') %>%
-  mutate(pred = if_else(below50 > .8, 'below50', 'over50') %>% factor) %>%
+  mutate(pred = if_else(below50 > threshold, 'below50', 'over50') %>% factor) %>%
   mutate(obs = xytest$class %>% factor)
-
-testPredict <- testPredict %>%
-  mutate(pred = if_else(below50 > threshold, 'below50', 'over50') %>% factor)
+########### See confusion matrix
 confusionMatrix(testPredict$obs, testPredict$pred)
-
+########### and ROC curve
 pROC_obj_cal <- roc(testPredict$obs, testPredict$below50, ci = TRUE)
 pROC_obj_cal
 plot(pROC_obj_cal)
